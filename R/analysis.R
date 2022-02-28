9009c12157647e687d097bb68cdcb8378514f2d6
Etienne Camenen <etienne.camenen@gmail.com>
2022-02-28 11:18:31 +0100

    Add fonctions to extract

--- R/analysis.R
+++ R/analysis.R
@@ -1,12 +1,16 @@
-var_names <- c("contour_px", "interior_px", "perimeter_um", "area_um2")
-cols <- paste(c("mean", "median", "sd"), rep(var_names, each = 3), sep = "_")
+cols <- paste0(
+    c("mean_", "median_", "sd_"),
+    rep(
+        c(var_names, paste0(var_names, "_weighted")),
+        each = 3)
+    )
 
 # load(paste0(file.path(path, file), n_size, ".RData"))
 # data <- read.xlsx(file.path(path, file))
 # data[, 1] <- convert_to_date(data[, 1])
 
 data <- data.frame(data)
-for (i in 2:8)
+for (i in 2:7)
     data[, i] <- as.factor(data[, i])
 
 list_tables <- list(data)
@@ -23,7 +27,7 @@ calculate_stats <- function(df) {
     }
     unlist(
         lapply(
-            var_names,
+            c(var_names, paste0(var_names, "_weighted")),
             function(k)
                 sapply(
                     c(f1, f2, sd),
@@ -41,24 +45,63 @@ for (m in types) {
     for (d in diseases) {
         bs <- unique(data[data$disease_grp == d, ]$brain_id)
         for (b in bs) {
-            rs <- sort(unique(data[data$brain_id == b, ]$roi_id))
+            rs <- sort(unique(data[data$brain_id == b, ]$image_id))
             for (r in rs) {
-                # nb_cells <- max(as.integer(df$cell_id))
                 i <- i + 1
-                df <- data[
-                    data$disease_grp == d &
+                condition <- data$disease_grp == d &
                     data$brain_id == b &
-                    data$roi_id == r &
-                    data$modality == m, ]
-                nb_cells <- sapply(seq(3), count_poncta)
-                tables[[i]] <- c(d, b, r, m, t(nb_cells), sum(nb_cells), calculate_stats(df))
+                    data$image_id == r
+
+                df <- data[condition & data$modality == m, ]
+                df_contour <- data[condition & data$modality == "contour", ]
+
+                nb_cells <- sapply(seq(4), count_poncta)
+                nb_poncta_weighted <- Reduce(
+                    cbind,
+                    lapply(nb_cells, function(x) x/roi)
+                )
+                sum_poncta_weighted <- sapply(
+                    var_names,
+                    function(v) sum(nb_poncta_weighted[names(nb_poncta_weighted) == v])
+                )
+
+                for (c in df_contour$cell_id) {
+                    roi <- df_contour[df_contour$cell_id == c, var_names]
+                    row_match <- condition &
+                        data$modality == m &
+                        data$cell_id == c
+                    for (v in var_names) {
+                        list_tables[[1]][
+                            row_match, paste0(v, "_weighted")
+                        ] <- list_tables[[1]][row_match, v] / roi[, v]
+                    }
+                }
+
+                stats <- calculate_stats(list_tables[[1]][condition & data$modality == m, ])
+                tables[[i]] <- c(d, b, r, m, t(nb_cells), sum(nb_cells), as.matrix(nb_poncta_weighted), sum_poncta_weighted, sum(nb_cells > 0), stats)
             }
         }
     }
 }
 
+cols_poncta <- c(
+    paste0(
+        rep(paste0("nb_poncta_", seq(4)),  each = 4),
+        rep(paste0(var_names, "_weighted"), 4)
+    ),
+    paste0("nb_poncta_tot", c("", "_weighted"))
+)
+
+cols_p_weight <- paste(
+    rep(paste0("nb_poncta_", seq(4)),  each = 4),
+    rep(paste0(var_names, "_weighted"), 4),
+    sep = "_"
+)
+
+cols_s_p_weight <- paste0("sum_poncta_weighted_", var_names)
+
 list_tables[[2]] <- data.frame(Reduce(rbind, tables))
-colnames(list_tables[[2]]) <- c(keys[4:6], keys[8], paste0("nb_poncta_", seq(3)), "nb_poncta_tot", cols)
+colnames(list_tables[[2]]) <- c(keys[4:6], keys[8], paste0("nb_poncta_", seq(4)), "sum_poncta", cols_p_weight, cols_s_p_weight, "nb_cells", cols)
 
 tables <- list()
 i <- 0
@@ -66,19 +109,24 @@ for (m in types) {
     for (d in diseases) {
         bs <- unique(data[data$disease_grp == d, ]$brain_id)
         for (b in bs) {
-            rs <- sort(unique(data[data$brain_id == b, ]$roi_id))
+            rs <- sort(unique(data[data$brain_id == b, ]$image_id))
             i <- i + 1
+            condition <- c(keys[4:5], keys[8])
             df <- data[
                 data$disease_grp == d &
                 data$brain_id == b &
                 data$modality == m, ]
-            tables[[i]] <- c(d, b, m, length(rs), calculate_stats(df))
+            nb_poncta <- sum(list_tables[[2]][
+                list_tables[[2]]$disease_grp == d &
+                list_tables[[2]]$brain_id == b &
+                list_tables[[2]]$modality == m, ]$nb_poncta_tot) # nb_poncta_ponderated
+            tables[[i]] <- c(d, b, m, length(rs), nb_cells, calculate_stats(df))
         }
     }
 }
 
 list_tables[[3]] <- data.frame(Reduce(rbind, tables))
-colnames(list_tables[[3]]) <- c(keys[4:5], keys[8], "nb_cells", cols)
+colnames(list_tables[[3]]) <- c(keys[4:5], keys[8], "nb_images", "nb_cells", cols)
 
 tables <- list()
 i <- 0
@@ -94,9 +142,9 @@ for (m in types) {
 }
 
 list_tables[[4]] <- data.frame(Reduce(rbind, tables))
-colnames(list_tables[[4]]) <- c(keys[4], keys[8], "nb_patients", cols)
+colnames(list_tables[[4]]) <- c(keys[4], keys[8], "nb_brains", cols)
 
-sheets <- c("Raw", "Cells", "Patients", "Groups")
+sheets <- c("Raw", "Images", "Brains", "Groups")
 wb <- createWorkbook()
 for (i in seq_along(sheets)) {
     addWorksheet(wb, sheets[i])
--- R/extract_roi.R
+++ R/extract_roi.R
@@ -10,7 +10,7 @@ library(parallel)
 pathos <- c(c("CTRL", "Crtl", "Ctrl", "ctrl"), "[sS]AD", "D[uU][pP][- ]?APP", "APP(?:mutation)?", "APP point mutation", "DS", "DS-D")
 pathos <- paste(pathos, collapse = "|")
 exp_reg <- paste0("[-_](", pathos, ")[-_ ](.+)")
-meta_reg <- "(^\\d{8})-(IHC) (H\\d{3})(.*)(\\.lif(.*))?(\\d{1,2})(?:\\.cells)? ?[-_]+ICY( -CELL_(\\d))?(_DATA)?_([-\\w]+).xlsx$"
+meta_reg <- "(^\\d{8})-(IHC) (H\\d{3})(.*)(\\.lif(.*))? ?- ?(?:Series0)?(\\d{1,2})(?:\\.cells)? ?[-_]+ICY( -CELL_(\\d))?(_DATA)?_([-\\w]+).xlsx$"
 
 extract_metada <- function(file_name) {
     metadatas <- str_replace(
@@ -21,15 +21,17 @@ extract_metada <- function(file_name) {
     (metadatas <- str_split(metadatas, ";")[[1]])
 
     exp <- gsub(exp_reg, "\\1;\\2", metadatas[4])
-    exp <- gsub("(\\.lif ?)?- ?(Series0)?", "", exp)
+    exp <- gsub("(\\.lif ?)|(Series0)|(mutation)|[-_ .]|(point)", "", exp)
     exp <- str_split(exp, ";")[[1]]
 
-    grp <- toupper(gsub("mutation| |point", "", exp[1]))
+    grp <- toupper(gsub("", "", exp[1]))
     if (grp == "CRTL")
         grp <- "CTRL"
-    brain_id <- gsub("[.]", "_", exp[2])
+    brain_id <- exp[2]
     if (brain_id == "BBN9608")
-        brain_id <- "A5197_BBN9608"
+        brain_id <- "A5197BBN9608"
+    if (metadatas[3] == "H038" && brain_id == "IB6125")
+        brain_id <- "IB6125-9"
 
     c(metadatas[seq(3)], grp, brain_id, metadatas[5:7])
 }
@@ -45,14 +47,14 @@ types_to_modify <- c("CONTOUR", "All-CathepsinB", "Exclusive-CathepsinB", "Minus
 types_modified <- c("contour", "all CATHB", "excl CATHB", "minus LF", "LF")
 
 var_names <- c("contour_px", "interior_px", "perimeter_um", "area_um2")
-keys <- c("date", "analysis", "exp_id", "disease_grp", "brain_id", "roi_id", "cell_id", "modality")
+keys <- c("date", "analysis", "exp_id", "disease_grp", "brain_id", "image_id", "cell_id", "modality")
 col_names <- c(keys[seq(6)], var_names, keys[7:8])
 
 CST <- 0.08154
 
 format_icy <- function(file_name) {
 
-    df <- read.xlsx(file.path(path, folder, file_name))
+    df <- read_xlsx(file.path(path, folder, file_name))
     df <- clean_names(df)
 
     metadatas <- extract_metada(file_name)
@@ -68,14 +70,13 @@ format_icy <- function(file_name) {
     }
 
     df$date <- convert_to_date(df$date)
-    df$roi_id <- as.integer(df$roi_id)
+    df$image_id <- as.integer(df$image_id)
     df$perimeter_um <- df$contour_px * CST
     df$area_um2 <- df$interior_px * CST^2
     df$dataset <- NULL
     df$name <- NULL
-    df$name <- analysis
 
-    df[, c(keys, var_names)]
+    df[, c(keys[-2], var_names)]
 }
 
 files <- mclapply(
